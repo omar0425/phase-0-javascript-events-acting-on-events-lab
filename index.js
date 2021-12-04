931dc50b82342491475cb8ec3a89e9e57512da5d
Omar Razi <startfresh87@gmail.com>
2021-12-04 12:26:27 -0500

    commit tree ca512cdaa43f0736c7010dd2db0eba5f60c24d77

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+// // Your code here
+// const dodger = document.getElementById("dodger");
+// dodger.style.backgroundColor = "#ff69b4";
+// dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", " ");
+  const left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight(){
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }else if(e.key === "ArrowRight"){
+    moveDodgerRight()
+  }
+});
